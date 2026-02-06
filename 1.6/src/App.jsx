d73f9a77bd034dbea27f4397c726cc646b953ec1
AkseliRaj <akseli.rajahalme@gmail.com>
2026-02-06 13:57:43 +0200

    1.13.

--- 1.6/src/App.jsx
+++ 1.6/src/App.jsx
@@ -6,72 +6,44 @@ const Button = (props) => (
   </button>
 )
 
-const StatisticLine = (props) => (
-  <tr>
-    <td>{props.text}</td>
-    <td>{props.value}</td>
-  </tr>
-)
-
-
-const Statistics = (props) => {
-  if (props.total === 0) {
-    return (
-      <div>
-        <h1>Statistics</h1>
-        <p>No feedback given</p>
-      </div>
-    )
-  }
-  return (
-    <div>
-      <h1>Statistics</h1>
-
-      <table>
-        <tbody>
-          <StatisticLine text="good" value={props.good} />
-          <StatisticLine text="neutral" value={props.neutral} />
-          <StatisticLine text="bad" value={props.bad} />
-          <StatisticLine text="all" value={props.total} />
-          <StatisticLine text="average" value={props.average} />
-          <StatisticLine
-            text="positive"
-            value={`${props.positivePercentage} %`}
-          />
-        </tbody>
-      </table>
-    </div>
-  )
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max)
 }
 
-
 const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
-  const total = good + neutral + bad
-  const average = total === 0 ? 0 : (good - bad) / total
-  const positivePercentage = total === 0 ? 0 : (good / total) * 100
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+
+  const nextAnecdote = () => {
+    const randomIndex = getRandomInt(anecdotes.length)
+    setSelected(randomIndex)
+  }
+
+  const voteAnecdote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    console.log({selected}, "on kasvatetu yhdellä")
+    setVotes(copy)
+
+  }
 
   return (
     <div>
-      <div>
-        <h1>Give feedback</h1>
-        <Button onClick={() => setGood(good + 1)} text="good" />
-        <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
-        <Button onClick={() => setBad(bad + 1)} text="bad" />
-      </div>
-
-      <Statistics
-        good={good}
-        neutral={neutral}
-        bad={bad}
-        total={total}
-        average={average}
-        positivePercentage={positivePercentage}
-      />
+      <div>{anecdotes[selected]}</div>
+      <div>has {votes[selected]} votes</div>
+      <Button onClick={voteAnecdote} text="vote" />
+      <Button onClick={nextAnecdote} text="next anecdote" />
     </div>
   )
 }
